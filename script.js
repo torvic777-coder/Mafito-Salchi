3deb332cca04085fb3dac341b3c7a9c179f7856b
torvic777-coder <victorospina363@gmail.com>
2025-01-12 18:31:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>      
+function mostrar(){
+    document.getElementById("img").style.display = "block"}
+
+    function ocultar(){
+        document.getElementById("img").style.display = "none"}</script>
